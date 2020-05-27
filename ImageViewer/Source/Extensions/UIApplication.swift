01bd54bbd860eef77b6e249ceff811624e9d01e8
leeweisern <leeweisern@gmail.com>
2020-05-27 16:32:41 +0800

    Handle iOS 13 uses scenedelegate for application window

--- ImageViewer/Source/Extensions/UIApplication.swift
+++ ImageViewer/Source/Extensions/UIApplication.swift
@@ -9,15 +9,24 @@
 import UIKit
 
 extension UIApplication {
-
+    
     static var applicationWindow: UIWindow {
-        return UIApplication.shared.keyWindow!
+        if #available(iOS 13.0, *) {
+            return (UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .map({$0 as? UIWindowScene})
+                .compactMap({$0})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first)!
+        } else {
+            return UIApplication.shared.keyWindow!
+        }
     }
-
+    
     static var isPortraitOnly: Bool {
-
+        
         let orientations = UIApplication.shared.supportedInterfaceOrientations(for: nil)
-
+        
         return !(orientations.contains(.landscapeLeft) || orientations.contains(.landscapeRight) || orientations.contains(.landscape))
     }
 }
